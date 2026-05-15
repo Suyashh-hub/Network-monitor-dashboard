27b27bbcaa479549751ebb897f33d77cf0827ce4
Suyash Nandeshwar <suyash@LAPTOP-5D0V7J5L.localdomain>
2026-05-15 13:32:12 +0000

    Phase 2 complete - added SSH server monitoring

--- app.py
+++ app.py
@@ -1,25 +1,53 @@
 from flask import Flask, render_template
 import json
-from monitor import ping_host
+
+from monitor import (
+    ping_host,
+    get_server_stats
+)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def dashboard():
+
     with open("devices.json") as f:
         devices = json.load(f)
 
     results = []
 
-    for d in devices:
-        status = ping_host(d["ip"])
-        results.append({
-            "name": d["name"],
-            "ip": d["ip"],
-            "status": status
-        })
+    for device in devices:
 
-    return render_template("dashboard.html", devices=results)
+        status = ping_host(device["ip"])
+
+        device_data = {
+            "name": device["name"],
+            "ip": device["ip"],
+            "status": status
+        }
+
+        if (
+            status == "UP"
+            and "username" in device
+            and "password" in device
+        ):
+
+            stats = get_server_stats(
+                device["ip"],
+                device["username"],
+                device["password"]
+            )
+
+            device_data.update(stats)
+
+        results.append(device_data)
+
+    return render_template(
+        "dashboard.html",
+        devices=results
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- monitor.py
+++ monitor.py
@@ -1,5 +1,44 @@
 import os
+import paramiko
 
 def ping_host(ip):
     response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
     return "UP" if response == 0 else "DOWN"
+
+
+def get_server_stats(ip, username, password):
+    try:
+        ssh = paramiko.SSHClient()
+
+        ssh.set_missing_host_key_policy(
+            paramiko.AutoAddPolicy()
+        )
+
+        ssh.connect(
+            hostname=ip,
+            username=username,
+            password=password,
+            timeout=5
+        )
+
+        commands = {
+            "cpu": "top -bn1 | grep 'Cpu' | awk '{print $2 + $4 \"%\"}'",
+            "ram": "free -m | awk 'NR==2{printf \"%.2f%%\", $3*100/$2}'",
+            "disk": "df -h / | awk 'NR==2 {print $5}'"
+        }
+
+        stats = {}
+
+        for key, command in commands.items():
+            stdin, stdout, stderr = ssh.exec_command(command)
+
+            stats[key] = stdout.read().decode().strip()
+
+        ssh.close()
+
+        return stats
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
